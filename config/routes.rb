57699dbe444dff3833d33381f8fc32675aebe207
wanbanka <32719584+wanbanka@users.noreply.github.com>
2019-03-21 21:52:43 +0100

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
     resources :futurs
   resources :themes
   resources :decisions
     get 'choice/:id' => 'main#choice'
+    get 'predictun/:theme/:id' => 'main#predictun'
+    get 'predictdeux/:theme/:id' => 'main#predictdeux'
+    get 'conclude/:theme/:id' => 'main#conclude'
+    get 'submit' => 'main#submit'
     root 'main#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
